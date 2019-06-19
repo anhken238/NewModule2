91e3a560141362b7574bc7c7f49e4208e17459fb
anhken238 <anhken238@gmail.com>
2019-06-19 21:34:46 +0700

    add file

---
+++ session7/MylinkedList.java
@@ -0,0 +1,72 @@
+package session7;
+
+class MyLinkedList {
+    private Node head;
+    private int numNodes;
+
+    public MyLinkedList(Object data) {
+        head = new Node(data);
+    }
+
+    private class Node {
+        private Node next;
+        private Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData() {
+            return this.data;
+        }
+    }
+
+    public void add(int index, Object data) {
+        Node temp = head;
+        Node holder;
+
+        for (int i = 0; i < index - 1 && temp.next != null; i++) {
+            temp = temp.next;
+        }
+
+        holder = temp.next;
+        temp.next = new Node(data);
+        temp.next.next = holder;
+        numNodes++;
+    }
+
+    public void addFirst(Object data) {
+        Node temp = head;
+        head = new Node(data);
+        head.next = temp;
+        numNodes++;
+    }
+
+    public Node get(int index) {
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(8);
+        ll.addFirst(9);
+        ll.addFirst(10);
+        ll.addFirst(11);
+
+        ll.add(5,7);
+        ll.add(5,7);
+        ll.printList();
+    }
+}
