a9d9bdc140ab84be099352c62c99e31f0e2b86f1
anhken238 <anhken238@gmail.com>
2019-06-20 10:36:36 +0700

    add file ss9

---
+++ session9/thucthicaulenhcrud.sql
@@ -0,0 +1,47 @@
+use mydata;
+select * from customers;
+select customernumber, contactlastname, contactfirstname, phone, city 
+from customers;
+
+select customernumber, contactlastname, contactfirstname, phone, city 
+from customers 
+where contactfirstname = 'Peter' and city = 'Melbourne';
+
+select customernumber, contactlastname, contactfirstname, phone, city 
+from customers 
+where contactfirstname like '%A%';
+
+select customernumber, contactlastname, contactfirstname, phone, city 
+from customers 
+where customernumber between 103 and 125;
+
+select customernumber, contactlastname, contactfirstname, phone, city 
+from customers 
+where city in ('USA', 'France', 'Spain') ;
+
+select customernumber, contactlastname, contactfirstname, phone, city 
+from customers 
+where country = 'USA' or country = 'France';
+
+select customernumber, contactlastname, contactfirstname, phone, city 
+from customers 
+order by contactfirstname;
+
+select  customernumber, contactlastname, contactfirstname, phone, city 
+from customers 
+order by contactfirstname desc
+limit 10;
+
+select count(country) as 'Số khách hàng số ở France' 
+from customers 
+where city = 'France' ;
+
+insert into customers (customerNumber, customerName, contactfirstName, contactlastName, phone, addressline1, city, country) 
+values (1, 'AgileLead', 'Lan', 'Trần', '0978822683', 'Hàm Nghi', 'Hà Nội', 'Việt Nam' );
+
+update customers 
+set customername = 'Baane Mini Imports'
+where customernumber = 103;
+
+DELETE FROM customers
+WHERE city = 'Nantes';
